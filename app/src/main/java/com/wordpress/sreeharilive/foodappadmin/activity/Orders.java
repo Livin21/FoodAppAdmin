287f517f44cb30aced5e9318d18b53cc67de72e6
livin <livinmathew99@gmail.com>
2017-04-02 15:41:52 +0530

    View Orders

--- app/src/main/java/com/wordpress/sreeharilive/foodappadmin/activity/Orders.java
+++ app/src/main/java/com/wordpress/sreeharilive/foodappadmin/activity/Orders.java
@@ -3,13 +3,17 @@ package com.wordpress.sreeharilive.foodappadmin.activity;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.wordpress.sreeharilive.foodappadmin.R;
+import com.wordpress.sreeharilive.foodappadmin.adapter.OrdersAdapter;
 import com.wordpress.sreeharilive.foodappadmin.model.FoodItem;
 import com.wordpress.sreeharilive.foodappadmin.model.Order;
 
@@ -22,12 +26,17 @@ public class Orders extends AppCompatActivity {
 
     ArrayList<Order> orders;
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
 
-        orders = new ArrayList<>();
+        recyclerView = (RecyclerView) findViewById(R.id.ordersRecyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
         fetchOrders();
 
@@ -44,32 +53,36 @@ public class Orders extends AppCompatActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
 
+                        orders = new ArrayList<>();
+
                         for (DataSnapshot child : dataSnapshot.getChildren()){
-                            String orderId = child.getKey();
-                            String address = child.child("address").getValue().toString();
-                            String locality = child.child("locality").getValue().toString();
-                            long timestamp = Long.parseLong(child.child("timestamp").getValue().toString());
-                            String userId = child.child("userId").getValue().toString();
-                            double total = Double.parseDouble(child.child("order").child("total").getValue().toString());
-                            String modeOfPayment = child.child("mode_of_payment").getValue().toString();
-                            ArrayList<FoodItem> items = new ArrayList<>();
+                            try{
+                                String orderId = child.getKey();
+                                String address = child.child("address").getValue().toString();
+                                String locality = child.child("locality").getValue().toString();
+                                long timestamp = Long.parseLong(child.child("timestamp").getValue().toString());
+                                String userId = child.child("userId").getValue().toString();
+                                double total = Double.parseDouble(child.child("order").child("total").getValue().toString());
+                                String modeOfPayment = child.child("mode_of_payment").getValue().toString();
+                                ArrayList<FoodItem> items = new ArrayList<>();
                                 for (DataSnapshot item : child.child("order").getChildren()){
                                     if (!item.getKey().equals("total")){
-                                    String category = item.child("category").getValue().toString();
-                                    String fid = item.child("fid").getValue().toString();
-                                    String itemName = item.child("item").getValue().toString();
-                                    int qty = Integer.parseInt(item.child("quantity").getValue().toString());
-                                    items.add(new FoodItem(
-                                            itemName,
-                                            category,
-                                            fid,
-                                            qty
-                                    ));
+                                        String category = item.child("category").getValue().toString();
+                                        String fid = item.child("fid").getValue().toString();
+                                        String itemName = item.child("item").getValue().toString();
+                                        int qty = Integer.parseInt(item.child("quantity").getValue().toString());
+                                        items.add(new FoodItem(
+                                                itemName,
+                                                category,
+                                                fid,
+                                                qty
+                                        ));
                                     }
                                 }
 
-                            Order order = new Order(orderId,address,locality,userId,total,items,timestamp,modeOfPayment);
-                            orders.add(order);
+                                Order order = new Order(orderId,address,locality,userId,total,items,timestamp,modeOfPayment);
+                                orders.add(order);
+                            }catch (NullPointerException ignored){}
                         }
                         Log.d("Orders",orders.toString());
 
@@ -87,12 +100,20 @@ public class Orders extends AppCompatActivity {
                                 }
                         );
 
+
+
+                        recyclerView.swapAdapter(
+                                new OrdersAdapter(Orders.this, orders),
+                                true
+                        );
+
                         progressDialog.dismiss();
 
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(Orders.this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
                         progressDialog.dismiss();
                     }
                 });
--- app/src/main/java/com/wordpress/sreeharilive/foodappadmin/adapter/OrdersAdapter.java
+++ app/src/main/java/com/wordpress/sreeharilive/foodappadmin/adapter/OrdersAdapter.java
@@ -1,5 +1,59 @@
 package com.wordpress.sreeharilive.foodappadmin.adapter;
 
 
-public class OrdersAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.wordpress.sreeharilive.foodappadmin.R;
+import com.wordpress.sreeharilive.foodappadmin.model.Order;
+
+import java.util.ArrayList;
+
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.ViewHolder>{
+
+    private ArrayList<Order> orders;
+    private Context context;
+
+    public OrdersAdapter(Context context, ArrayList<Order> orders) {
+        this.orders = orders;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.orders_list_item,parent,false);
+        return new OrdersAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.addressTV.setText(orders.get(position).getAddress());
+        holder.localityTV.setText(orders.get(position).getLocality());
+        holder.userIdTV.setText(orders.get(position).getUserID());
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView addressTV;
+        TextView localityTV;
+        TextView userIdTV;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            addressTV = (TextView) itemView.findViewById(R.id.userAddressTextView);
+            localityTV = (TextView) itemView.findViewById(R.id.userLocalityTextView);
+            userIdTV = (TextView) itemView.findViewById(R.id.userIDTextView);
+        }
+    }
 }
