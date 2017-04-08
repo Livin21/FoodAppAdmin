6f29c70aad08639928746e5f7c605ea563abf164
livin <livinmathew99@gmail.com>
2017-04-08 23:48:45 +0530

    Working on Notifications

---
+++ app/src/main/java/com/wordpress/sreeharilive/foodappadmin/service/OrderReceiverService.java
@@ -0,0 +1,20 @@
+package com.wordpress.sreeharilive.foodappadmin.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class OrderReceiverService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+
+        return START_STICKY;
+    }
+}
